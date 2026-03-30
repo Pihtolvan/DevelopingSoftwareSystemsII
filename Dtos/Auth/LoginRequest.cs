d89ec0a9beacfde7b5d9deea04aed4e8f6cb672d
Pihtolvan <pianunien@gmail.com>
2026-03-30 21:42:25 +0300

    add auth validation

--- Dtos/Auth/LoginRequest.cs
+++ Dtos/Auth/LoginRequest.cs
@@ -1,7 +1,15 @@
-﻿namespace Todo.Api.Dtos.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Api.Dtos.Auth;
 
 public class LoginRequest
 {
+    [Required]
+    [EmailAddress]
+    [StringLength(254)]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(128, MinimumLength = 6)]
     public string Password { get; set; } = string.Empty;
 }
--- Dtos/Auth/RegisterRequest.cs
+++ Dtos/Auth/RegisterRequest.cs
@@ -1,8 +1,17 @@
-﻿namespace Todo.Api.Dtos.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Api.Dtos.Auth;
 
 public class RegisterRequest
 {
+    [Required]
+    [EmailAddress]
+    [StringLength(254)]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(128, MinimumLength = 6)]
     public string Password { get; set; } = string.Empty;
+
     public string? DisplayName { get; set; }
 }
